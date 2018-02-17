915324bed4ae016061d7845fef30132080fa929e
ziyan <zjames1@vt.edu>
2018-02-17 14:11:36 -0500

    more info

--- app/src/main/java/c/braedenm/dashesinadot/MainActivity.java
+++ app/src/main/java/c/braedenm/dashesinadot/MainActivity.java
@@ -1,7 +1,14 @@
 package c.braedenm.dashesinadot;
 
+import android.content.Context;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -11,34 +18,46 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity
 {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference reference = database.getReference("test");
+
+    Button b_vibe;
+    Vibrator v;
+
+/**
+ * Links with the activity_main and controls its activities
+ */
+public class MainActivity extends AppCompatActivity
+{
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
     private DatabaseReference reference = database.getReference("test");
+    private DatabaseReference person = database.getReference("person1");
+    private TextView readout;
+    private Button button;
+    private Boolean active;
 
-    @Override
     /**
-     * Main method
+     * This is called when the application loads activity_main
+     * @param savedInstanceState Previous instance of the program
      */
+    @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         reference.setValue("quack");
 
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot)
-            {
-                String value = dataSnapshot.getValue(String.class);
-                System.out.println(value);
-            }
+        //b_vibe = (Button) findViewById(R.id.button2);
+        v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 
+        b_vibe.setOnClickListener (new View.OnClickListener()
+        {
             @Override
-            public void onCancelled(DatabaseError databaseError)
+            public void onClick(View view)
             {
-                System.out.println("It failed bithces");
+                //vibrate in ms
+                v.vibrate(100);
             }
         });
-
-        reference.setValue("hello");
     }
 }
