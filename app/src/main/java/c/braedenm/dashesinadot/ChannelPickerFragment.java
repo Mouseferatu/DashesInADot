93c0e0c976166d0e15464e5610bd94c717cb06a0
ziyan <zjames1@vt.edu>
2018-02-17 14:09:53 -0500

    store info

---
+++ app/src/main/java/c/braedenm/dashesinadot/ChannelPickerFragment.java
@@ -0,0 +1,19 @@
+package c.braedenm.dashesinadot;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+
+/**
+ * Created by James Zhang on 2/17/2018.
+ */
+
+public class ChannelPickerFragment extends DialogFragment
+{
+    public Dialog onCreateDialog(Bundle savedInstanceState)
+    {
+        return new Dialog();
+    }
+}
--- app/src/main/java/c/braedenm/dashesinadot/MainActivity.java
+++ app/src/main/java/c/braedenm/dashesinadot/MainActivity.java
@@ -3,19 +3,42 @@ package c.braedenm.dashesinadot;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity
 {
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference reference = database.getReference("test");
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference reference = database.getReference("test");
 
     @Override
+    /**
+     * Main method
+     */
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         reference.setValue("quack");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                String value = dataSnapshot.getValue(String.class);
+                System.out.println(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError)
+            {
+                System.out.println("It failed bithces");
+            }
+        });
+
+        reference.setValue("hello");
     }
 }
