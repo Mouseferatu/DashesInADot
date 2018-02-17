997768df7243f4afca281aeef7a53c5101f3e139
braedenm <braeden.muller@verizon.net>
2018-02-17 11:32:26 -0500

    Fixed UI overlap for Pixel 2. Testing functionality with S6.

--- app/src/main/java/c/braedenm/dashesinadot/MainActivity.java
+++ app/src/main/java/c/braedenm/dashesinadot/MainActivity.java
@@ -6,12 +6,21 @@ import android.os.Bundle;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity {
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference reference = database.getReference("test");
+/**
+ * Links with the activity_main and controls its activities
+ */
+public class MainActivity extends AppCompatActivity
+{
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference reference = database.getReference("test");
 
+    /**
+     * This is called when the application laods activity_main
+     * @param savedInstanceState Previous instance of the program
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         reference.setValue("quack");
