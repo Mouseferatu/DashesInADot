5545598d09822fdb7f299f0b2bef39ec4df092fd
braedenm <braeden.muller@verizon.net>
2018-02-17 01:22:24 -0500

    Added some initial debug sample code

--- app/src/main/java/c/braedenm/dashesinadot/MainActivity.java
+++ app/src/main/java/c/braedenm/dashesinadot/MainActivity.java
@@ -3,11 +3,17 @@ package c.braedenm.dashesinadot;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference reference = database.getReference("test");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        reference.setValue("quack");
     }
 }
